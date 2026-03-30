a815d7e5a8d65a0eb7c15f7ced51a7682f22b950
Jang <dnfqhrjsgml@gmail.com>
2026-03-30 20:41:48 +0900

    풋터 기능 형성

--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -1,4 +1,13 @@
 package com.example.demo.home;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller  // 이 부분이 있는지 꼭 확인해 주세요!
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home/home"; // 방금 전까지 작업하신 home.html을 띄우는 역할
+    }
 }
