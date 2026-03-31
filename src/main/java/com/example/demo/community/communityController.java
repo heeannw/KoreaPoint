e6fcb01caea78035f1e43c112097e9082b7103fb
Jang <dnfqhrjsgml@gmail.com>
2026-03-31 21:19:27 +0900

    홈페이지 수정

--- src/main/java/com/example/demo/community/communityController.java
+++ src/main/java/com/example/demo/community/communityController.java
@@ -1,4 +1,13 @@
 package com.example.demo.community;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class communityController {
+
+    @GetMapping("/community")
+    public String community() {
+        return "community/community";
+    }
 }
